b6883298f7fa99bcf1573721b8568399a869eaed
chararchter <chararchter@pm.me>
2026-09-01 10:38:44 +0100

    add derive

--- src/entity/drug.rs
+++ src/entity/drug.rs
@@ -1,20 +1,22 @@
-package models.entities
+use async_graphql::SimpleObject;
+use serde::Deserialize;
 
-import play.api.libs.json._
-import slick.jdbc.GetResult
-import utils.db.DbJsonParser.fromPositionedResult
+// ---- models ----
 
-pub struct DrugReferences{
+#[derive(Debug, Clone, Deserialize, SimpleObject)]
+pub struct DrugReferences {
     source: String,
     ids: Vec<String>,
 }
 
-pub struct DrugLabelAndSource{
+#[derive(Debug, Clone, Deserialize, SimpleObject)]
+pub struct DrugLabelAndSource {
     label: String,
     source: String,
 }
 
-pub struct Drug{
+#[derive(Debug, Clone, Deserialize, SimpleObject)]
+pub struct Drug {
     id: String,
     name: String,
     synonyms: Vec<DrugLabelAndSource>,
@@ -27,10 +29,10 @@ pub struct Drug{
     molblock: Option<String>,
 }
 
-object Drug {
-  implicit val getResult: GetResult[Drug] = GetResult(fromPositionedResult[Drug])
-  implicit val DrugXRefImpW: OFormat[DrugReferences] = Json.format[DrugReferences]
-  implicit val drugLabelAndSourceImpF: OFormat[DrugLabelAndSource] = Json.format[DrugLabelAndSource]
-  implicit val drugImplicitR: Reads[Drug] = Json.reads[Drug]
-  implicit val drugImplicitW: OWrites[Drug] = Json.writes[Drug]
-}
+// object Drug {
+//   implicit val getResult: GetResult[Drug] = GetResult(fromPositionedResult[Drug])
+//   implicit val DrugXRefImpW: OFormat[DrugReferences] = Json.format[DrugReferences]
+//   implicit val drugLabelAndSourceImpF: OFormat[DrugLabelAndSource] =
+// Json.format[DrugLabelAndSource]   implicit val drugImplicitR: Reads[Drug] = Json.reads[Drug]
+//   implicit val drugImplicitW: OWrites[Drug] = Json.writes[Drug]
+// }
