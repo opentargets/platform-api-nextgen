612dfdf67b11ee91e5dc9debdb6f1e3f8904ab52
chararchter <chararchter@pm.me>
2026-09-02 10:05:32 +0100

    Original Scala code

---
+++ src/entity/drug_warning.rs
@@ -0,0 +1,37 @@
+package models.entities
+
+import utils.OTLogging
+import play.api.libs.json._
+import slick.jdbc.GetResult
+import utils.db.DbJsonParser.fromPositionedResult
+
+case class DrugWarningReference(id: String, source: String, url: String)
+
+case class DrugWarning(
+    toxicityClass: Option[String],
+    chemblIds: Option[Seq[String]],
+    country: Option[String],
+    description: Option[String],
+    id: Option[Long],
+    references: Option[Seq[DrugWarningReference]],
+    warningType: String,
+    year: Option[Int],
+    efoTerm: Option[String],
+    efoId: Option[String],
+    efoIdForWarningClass: Option[String]
+)
+
+case class DrugWarnings(
+    chemblId: String,
+    drugWarnings: Seq[DrugWarning]
+)
+
+object DrugWarning extends OTLogging {
+  implicit val getDrugWarningsFromDB: GetResult[DrugWarnings] =
+    GetResult(fromPositionedResult[DrugWarnings])
+  implicit val drugWarningsImpF: OFormat[DrugWarnings] = Json.format[models.entities.DrugWarnings]
+  implicit val drugWarningsReferenceImpF: OFormat[models.entities.DrugWarningReference] =
+    Json.format[models.entities.DrugWarningReference]
+  implicit val drugWarningImpF: OFormat[models.entities.DrugWarning] =
+    Json.format[models.entities.DrugWarning]
+}
