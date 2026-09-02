e2b6ed717778cfbca6ac700aef1fe86cb1c66a90
chararchter <chararchter@pm.me>
2026-09-02 13:38:09 +0100

    add comments to fields

--- src/entity/drug_warning.rs
+++ src/entity/drug_warning.rs
@@ -25,19 +25,31 @@ pub struct DrugWarningReference {
     url: String,
 }
 
+/// Blackbox and withdrawn information for drugs molecules included in ChEMBL database.
 #[derive(Debug, Clone, Deserialize, SimpleObject)]
 #[serde(rename_all = "camelCase")]
 pub struct DrugWarning {
+    /// Classification of toxicity type associated with the drug.
     toxicity_class: Option<String>,
+    /// List of Open Targets molecule identifiers.
     chembl_ids: Vec<String>,
+    /// Country where the warning was issued.
     country: Option<String>,
+    /// Description of the drug adverse effect.
     description: Option<String>,
+    /// Internal identifier for the drug warning record.
     id: Option<i64>,
+    /// List of sources supporting the warning information.
     references: Vec<DrugWarningReference>,
+    /// Classification of action taken (drug is withdrawn or has a black box warning).
     warning_type: String,
+    /// Year when the warning was issued.
     year: Option<i32>,
+    /// List of disease labels.
     efo_term: Option<String>,
+    /// List of disease identifiers.
     efo_id: Option<String>,
+    /// Disease identifier categorising the type of warning.
     efo_id_for_warning_class: Option<String>,
 }
 
