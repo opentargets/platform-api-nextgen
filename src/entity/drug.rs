d9f19675724a479c29ebce2a9a22a5b926bf4dbf
chararchter <chararchter@pm.me>
2026-09-08 15:59:19 +0100

    Paginate drug warnings

--- src/entity/drug.rs
+++ src/entity/drug.rs
@@ -222,7 +222,12 @@ impl Drug {
     }
 
     /// Warnings present on drug as identified by ChEMBL.
-    async fn drug_warnings(&self, ctx: &Context<'_>) -> async_graphql::Result<Vec<DrugWarning>> {
-        load_drug_warnings(ctx, &self.id).await
+    async fn drug_warnings(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(default)] page: Page,
+    ) -> async_graphql::Result<Paged<DrugWarning>> {
+        let items = load_drug_warnings(ctx, &self.id).await?;
+        Ok(items.query().paginate(page))
     }
 }
